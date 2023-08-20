1775fc545ffcd5d776712bc15deb33da126f7f13
hide1102 <136874206+hide1102@users.noreply.github.com>
2023-08-20 22:10:13 +0900

    カメラワーク３
    SubCameraを追加
    ３回PlayerがBoardに当たるとSubCameraに切り替わるように

---
+++ Assets/Script/CameraWork.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour
+{
+    private GameObject MainCamera;
+    private GameObject SubCamera;
+    private GameObject PM;
+    private TestPlayerMove PMCs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PM = GameObject.Find("Player");
+        PMCs = PM.GetComponent<TestPlayerMove>();
+        
+        MainCamera = GameObject.Find("Main Camera");
+        SubCamera = GameObject.Find("SubCamera");
+
+        SubCamera.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PMCs.Round >= 3)
+        {
+            MainCamera.SetActive(false);
+            SubCamera.SetActive(true);
+        }
+    }
+    
+}
