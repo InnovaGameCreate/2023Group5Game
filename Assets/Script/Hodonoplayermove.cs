48c037175a4cbd9a8074d8fa90ece743d082a863
HodonoYusuke0924 <yusukehdn@icloud.com>
2023-08-25 18:10:31 +0900

    程野シーン用移動スクリプト
    真スクリプトです（移動用）

---
+++ Assets/Script/Hodonoplayermove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hodonoplayermove : MonoBehaviour
+{
+    [SerializeField] private float MoveSpeed;
+    [SerializeField] private float xMax;
+    [SerializeField] private float xMin;
+    [SerializeField] private float yMax;
+    [SerializeField] private float yMin;
+    //private このスクリプト内でしか書き換えられない
+    //public どこからでも書き換えられる
+    //[SerializeFIeld] private unity上では変えられるけど他のスクリプトでは変えられない
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) && this.transform.position.x > xMin)//Aを押したら移動、かつオブジェクトのx座標がxMin以上の値なら移動可能
+            transform.Translate(new Vector3(-MoveSpeed, 0, 0) * Time.deltaTime); //移動するためのプログラム
+
+        if (Input.GetKey(KeyCode.D) && this.transform.position.x < xMax)//Dを押したら移動、かつオブジェクトのx座標がxMax以下の値なら移動可能
+            transform.Translate(new Vector3(MoveSpeed, 0, 0) * Time.deltaTime); //移動するためのプログラム
+
+        if (Input.GetKey(KeyCode.W) && this.transform.position.y < yMax)//Wを押したら移動、かつオブジェクトのz座標がzMax以下の値なら移動可能
+            transform.Translate(new Vector3(0, MoveSpeed, 0) * Time.deltaTime); //移動するためのプログラム
+
+        if (Input.GetKey(KeyCode.S) && this.transform.position.y > yMin)//Sを押したら移動、かつオブジェクトのz座標がxMin以上の値なら移動可能
+            transform.Translate(new Vector3(0, -MoveSpeed, 0) * Time.deltaTime); //移動するためのプログラム
+
+    }
+}
