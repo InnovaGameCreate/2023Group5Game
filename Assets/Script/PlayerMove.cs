a6a15ac7192914ccb3fe8a2b80126a26fa5e99cf
HodonoYusuke0924 <yusukehdn@icloud.com>
2023-08-26 15:02:50 +0900

    途中
    シーン遷移途中

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody rb;
     private bool isGravityActive = false;
     private bool isStuck = false;
-
+    private int boardCollisionCount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +23,8 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Playerが設定の高さから放物運動をする
-        if (!isGravityActive && transform.position.y> targetY)
+        // Playerが設定の高さから放物運動をする
+        if (!isGravityActive && transform.position.y > targetY)
         {
             isGravityActive = true;
             rb.useGravity = true;
@@ -31,14 +32,13 @@ public class PlayerMove : MonoBehaviour
 
         if (isStuck)
         {
-            //Boardに当たると止まる
+            // Boardに当たると止まる
             rb.velocity = Vector3.zero;
         }
         else
         {
-            //Playerを自動で進ませる
+            // Playerを自動で進ませる
             transform.Translate(new Vector3(xSpeed, ySpeed, zSpeed) * Time.deltaTime);
-
         }
     }
 
@@ -49,6 +49,18 @@ public class PlayerMove : MonoBehaviour
             isStuck = true;
             rb.velocity = Vector3.zero;
             rb.constraints = RigidbodyConstraints.FreezeAll;
+
+            // Boardにぶつかった回数をカウント
+            boardCollisionCount++;
+
+            // 3回ぶつかったら現在のシーンを再読み込み
+            if (boardCollisionCount >= 3)
+            {
+                // 現在のSceneを取得
+                Scene loadScene = SceneManager.GetActiveScene();
+                // 現在のシーンを再読み込みする
+                SceneManager.LoadScene(loadScene.buildIndex);
+            }
         }
     }
 }
