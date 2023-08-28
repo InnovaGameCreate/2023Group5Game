25be38077e3a027ee1067a27aa8b43d110768037
hide1102 <136874206+hide1102@users.noreply.github.com>
2023-08-28 23:25:59 +0900

    アスペクト比を統一

--- Assets/Script/BoardScore.cs
+++ Assets/Script/BoardScore.cs
@@ -6,6 +6,8 @@ public class BoardScore : MonoBehaviour
 {
     private GameObject SC;
     private ScoreCount SCCs;
+    private GameObject PM;
+    private TestPlayerMove PMCs;
     [SerializeField] private float boardscore;
     
     // Start is called before the first frame update
@@ -13,6 +15,8 @@ public class BoardScore : MonoBehaviour
     {
         SC = GameObject.Find("ScoreCounter");
         SCCs = SC.GetComponent<ScoreCount>();
+        PM = GameObject.Find("Player");
+        PMCs = PM.GetComponent<TestPlayerMove>();
     }
     
 
@@ -25,7 +29,9 @@ public class BoardScore : MonoBehaviour
     {
         if(other. gameObject. CompareTag("Player"))
         {
-           SCCs.score = boardscore;
+           if(PMCs.Round == 1){SCCs.firstScore = boardscore;}
+        if(PMCs.Round == 2){SCCs.scondScore = boardscore;}
+        if(PMCs.Round == 3){SCCs.thirdScore = boardscore;}
         }
     }
 }
