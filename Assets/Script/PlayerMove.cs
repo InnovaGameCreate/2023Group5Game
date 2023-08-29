aa1972b4156c6fd22492e091da5d75e968d5ef33
SatoshiFurutani <chips.potato.tomato@gmail.com>
2023-08-29 19:52:08 +0900

    2

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -5,18 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] public float xSpeed;
-    [SerializeField] public float ySpeed;
-    [SerializeField] public float zSpeed;
-    [SerializeField] public float xAirstoneForce;
-    [SerializeField] public float yAirstoneForce;
-    [SerializeField] public float zAirstoneForce;
-    [SerializeField] public float xFishForce;
-    [SerializeField] public float yFishForce;
-    [SerializeField] public float zFishForce;
+    [SerializeField] private float xSpeed;
+    [SerializeField] private float ySpeed;
+    [SerializeField] private float zSpeed;
+    [SerializeField] private float xFishForce;
+    [SerializeField] private float yFishForce;
+    [SerializeField] private float zFishForce;
     [SerializeField] private float targetY;
     [SerializeField] private float targetZ;
-    [SerializeField] private float accelerationAmount;
+    [SerializeField] private float upaccelerationAmount;
+    [SerializeField] private float forwardaccelerationAmount;
+
     private Rigidbody rb;
     private bool isGravityActive = false;
     private bool isStuck = false;
@@ -28,49 +27,43 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.useGravity = false; // Disable gravity at the start
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Playerが設定の高さから放物運動をする
-        if (!isGravityActive && transform.position.y > targetY || !isGravityActive && transform.position.z > targetZ)
+        if (!isGravityActive && (transform.position.y > targetY || transform.position.z > targetZ))
         {
             isGravityActive = true;
             rb.useGravity = true;
         }
 
-        if (isStuck)
+        if (!isStuck)
         {
-            // Boardに当たると止まる
-            rb.velocity = Vector3.zero;
-        }
-        else
-        {
-            // Playerを自動で進ませる
-            transform.Translate(new Vector3(xSpeed, ySpeed, zSpeed) * Time.deltaTime);
+            rb.velocity = new Vector3(xSpeed, ySpeed, zSpeed);
         }
 
-        // Playerの現在のxおよびz座標を取得
         float playerX = transform.position.x;
         float playerZ = transform.position.z;
-
-        // Airstoneのxおよびz座標を取得
         float airstoneX = airstoneTransform.position.x;
         float airstoneZ = airstoneTransform.position.z;
 
-        // PlayerがAirstoneのxおよびz座標上を通過した場合に加速する
-        if (playerX == airstoneX && playerZ == airstoneZ)
+        if (!isStuck && playerX >= airstoneX - 0.5f && playerX <= airstoneX + 0.5f &&
+            playerZ >= airstoneZ - 0.5f && playerZ <= airstoneZ + 0.5f)
         {
             Debug.Log("エアストーンの上を通過しました");
-            Vector3 force = new Vector3(xAirstoneForce, yAirstoneForce, zAirstoneForce);  // 力を設定
-            rb.AddForce(force, ForceMode.Impulse);          // 力を加える
+
+            Vector3 upAcceleration = transform.up * upaccelerationAmount; // 加速ベクトルをトリガーゾーンの上方向に設定
+            Vector3 forwardAcceleration = transform.forward * forwardaccelerationAmount;
+            Vector3 totalAcceleration = upAcceleration + forwardAcceleration;
+            rb.AddForce(totalAcceleration, ForceMode.Acceleration);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!isStuck && collision.gameObject.tag == "Board")
+        if (!isStuck && collision.gameObject.CompareTag("Board"))
         {
             isStuck = true;
             rb.velocity = Vector3.zero;
@@ -80,7 +73,6 @@ public class PlayerMove : MonoBehaviour
 
             if (boardCollisionCount >= MaxBoardCollisions)
             {
-                // SceneChangeスクリプトを持つゲームオブジェクトのSceneChangeコンポーネントを取得して、LoadEndSceneメソッドを呼び出す
                 SceneChange sceneChangeScript = FindObjectOfType<SceneChange>();
                 if (sceneChangeScript != null)
                 {
@@ -98,9 +90,8 @@ public class PlayerMove : MonoBehaviour
     {
         if (other.CompareTag("Fish"))
         {
-            Debug.Log("魚にぶつかりました");
-            Vector3 force = new Vector3(-xFishForce, -yFishForce, -zFishForce);  // 力を設定
-            rb.AddForce(force, ForceMode.Impulse);          // 力を加える
+            Vector3 force = new Vector3(-xFishForce, -yFishForce, -zFishForce);
+            rb.AddForce(force, ForceMode.Impulse);
         }
     }
 
