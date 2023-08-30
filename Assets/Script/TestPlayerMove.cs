d39ae5f1ba444cbd996aec15e697fdf3235c643a
HodonoYusuke0924 <yusukehdn@icloud.com>
2023-08-30 21:29:46 +0900

    Revert "シーン移行"
    This reverts commit 95c5cf73a16f0935a4605722fa31baae923cb790.

--- Assets/Script/TestPlayerMove.cs
+++ Assets/Script/TestPlayerMove.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // SceneManagerの名前空間をインポート
 using UnityEngine.UI;
 
 public class TestPlayerMove : MonoBehaviour
@@ -115,9 +114,5 @@ public class TestPlayerMove : MonoBehaviour
         yield return new WaitForSeconds(waittime);
         scText.SetActive(false);
         fscText.SetActive(true);
-
-        // 以下を追加して、一定の遅延後に「End」シーンに切り替える
-        yield return new WaitForSeconds(waittime); // 必要に応じて遅延を調整
-        SceneManager.LoadScene("End"); // 「End」シーンを読み込む
     }
 }
